fe8947e38c137cd6a5ef973a5206d403c270c3ff
fromeis <finn_johann.romeis@stud.htwk-leipzig.de>
2023-05-23 13:54:49 +0200

    Added UnitTestBeispiel.cs
    Test, der den WeatherForecast.cs testet, um die CI/CD Pipeline auszuprobieren

---
+++ UnitTestBeispiel.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace Backend_3.Tests
+{
+    public class WeatherForecastTests
+    {
+        [Test]
+        public void TemperatureF_Should_Convert_TemperatureC_Correctly()
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast
+            {
+                TemperatureC = 25
+            };
+
+            // Act
+            var temperatureF = weatherForecast.TemperatureF;
+
+            // Assert
+            Assert.AreEqual(77, temperatureF);
+        }
+    }
+}
