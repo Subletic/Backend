6031d299249d5d81f271830bf7b14527b0ebca15
Amine Jegani <amine.jegani@stud.htwk-leipzig.de>
2023-06-23 22:02:44 +0200

    #24 feat: create WebVttExporter and Construktor

---
+++ Backend/Services/WebVttExporter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Backend.Data;
+
+namespace Backend.Services;
+public class WebVttExporter
+{
+    private readonly Stream _outputStream;
+
+    public WebVttExporter(Stream outputStream)
+    {
+        _outputStream = outputStream;
+    }
+
+    public void ExportSpeechBubbles(List<SpeechBubble> speechBubbles)
+    {
+        string webVttContent = ConvertToWebVttFormat(speechBubbles);
+        WriteToStream(webVttContent);
+    }
+
+    private string ConvertToWebVttFormat(List<SpeechBubble> speechBubbles)
+    {
+    }
+
+
+    private void WriteToStream(string content)
+    {
+    }
+}
