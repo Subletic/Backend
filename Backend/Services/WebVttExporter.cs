29e43fb0ba9da9da756b4329535833ba5dbfa6d7
Amine Jegani <amine.jegani@stud.htwk-leipzig.de>
2023-06-23 22:45:05 +0200

    #24 fix: create functions of FormatTime, WriteTo Stream and ExportSpeechBubble

--- Backend/Services/WebVttExporter.cs
+++ Backend/Services/WebVttExporter.cs
@@ -1,30 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Backend.Data;
 
-namespace Backend.Services;
-public class WebVttExporter
+namespace Backend.Services
 {
-    private readonly Stream _outputStream;
 
-    public WebVttExporter(Stream outputStream)
+    /// <summary>
+    /// Class responsible for exporting speech bubbles to WebVTT format.
+    /// </summary>
+    public class WebVttExporter
     {
-        _outputStream = outputStream;
-    }
+        private readonly Stream _outputStream;
 
-    public void ExportSpeechBubbles(List<SpeechBubble> speechBubbles)
-    {
-        string webVttContent = ConvertToWebVttFormat(speechBubbles);
-        WriteToStream(webVttContent);
-    }
+        /// <summary>
+        /// Initializes a new instance of the WebVttExporter class with the specified output stream.
+        /// </summary>
+        /// <param name="outputStream">The output stream to write the WebVTT content to.</param>
+        public WebVttExporter(Stream outputStream)
+        {
+            _outputStream = outputStream;
+        }
 
-    private string ConvertToWebVttFormat(List<SpeechBubble> speechBubbles)
-    {
-    }
+        /// <summary>
+        /// Exports the speech bubbles to WebVTT format and writes the content to the output stream.
+        /// </summary>
+        /// <param name="speechBubbles">The list of speech bubbles to export.</param>
+        public void ExportSpeechBubbles(List<SpeechBubble> speechBubbles)
+        {
+            string webVttContent = ConvertToWebVttFormat(speechBubbles);
+            WriteToStream(webVttContent);
+        }
 
+        /// <summary>
+        /// Converts the speech bubbles to WebVTT format.
+        /// </summary>
+        /// <param name="speechBubbles">The list of speech bubbles to convert.</param>
+        /// <returns>The WebVTT-formatted content.</returns>
+        private string ConvertToWebVttFormat(List<SpeechBubble> speechBubbles)
+        {
+            // TODO: Implement the conversion logic
+        }
 
-    private void WriteToStream(string content)
-    {
+        /// <summary>
+        /// Formats the time in WebVTT format (hh:mm:ss.mmm).
+        /// </summary>
+        /// <param name="time">The time value to format.</param>
+        /// <returns>The formatted time string.</returns>
+        private string FormatTime(double time)
+        {
+            int hours = (int)(time / 3600);
+            int minutes = (int)((time % 3600) / 60);
+            int seconds = (int)(time % 60);
+            int milliseconds = (int)((time - Math.Truncate(time)) * 1000);
+
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}.{milliseconds:D3}";
+        }
+
+        /// <summary>
+        /// Writes the content to the output stream.
+        /// </summary>
+        /// <param name="content">The content to write.</param>
+        private void WriteToStream(string content)
+        {
+            using (var writer = new StreamWriter(_outputStream, Encoding.UTF8, 4096, true))
+            {
+                writer.Write(content);
+            }
+        }
     }
 }
