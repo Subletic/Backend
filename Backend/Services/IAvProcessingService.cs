41de8e31d8625ba917a07c64700cdebab6907287
cneidahl <christoph.neidahl@stud.htwk-leipzig.de>
2023-10-19 17:02:19 +0200

    #28 fix: Make Speechmatics communication compatible with enterprise key

--- Backend/Services/IAvProcessingService.cs
+++ Backend/Services/IAvProcessingService.cs
@@ -3,7 +3,7 @@ namespace Backend.Services;
 public interface IAvProcessingService
 {
     // apiKeyVar: envvar that contains the api key to send to speechmatics
-    public Task<bool> Init (string apiKeyVar);
+    public bool Init (string apiKeyVar);
 
     public Task<bool> TranscribeAudio (Uri mediaUri);
 }
