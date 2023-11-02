e0a44a7d6b2c44c25ae06cd0074d5f73f992fead
ajegani <145505424+ajegani@users.noreply.github.com>
2023-11-02 20:18:14 +0100

    #36 fix: comment code

--- BackendTests/SubtitleExporterTests.cs
+++ BackendTests/SubtitleExporterTests.cs
@@ -8,6 +8,10 @@ using Backend.Services;
 using Moq;
 using Xunit;
 
+/// <summary>
+/// Dies ist die Testklasse für die SubtitleExporter-Klasse.
+/// </summary>
+
 public class SubtitleExporterTests
 {
     [Fact]
@@ -47,7 +51,7 @@ public class SubtitleExporterTests
         // Überprüfen Sie, ob SendAsync mit den erwarteten Daten aufgerufen wurde
         mockWebSocket.Verify(
             webSocket => webSocket.SendAsync(
-                It.Is<ArraySegment<byte>>(buffer => Encoding.UTF8.GetString(buffer.Array) == expectedSubtitleText),
+                It.Is<ArraySegment<byte>>(buffer => buffer.Array != null && Encoding.UTF8.GetString(buffer.Array) == expectedSubtitleText),
                 WebSocketMessageType.Text,
                 true,
                 CancellationToken.None
